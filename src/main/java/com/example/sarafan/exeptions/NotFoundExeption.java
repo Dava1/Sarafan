02725a6000cbe2658a99bedae36632d864c26b79
Dava1 <davinci2189@gmail.com>
2021-03-31 00:23:19 +0200

    Dava1:added exeption class

--- src/main/java/com/example/sarafan/exeptions/NotFoundExeption.java
+++ src/main/java/com/example/sarafan/exeptions/NotFoundExeption.java
@@ -1,4 +1,8 @@
 package com.example.sarafan.exeptions;
 
-public class NotFoundExeption {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundExeption extends RuntimeException{
 }
