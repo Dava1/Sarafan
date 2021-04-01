597446a72f470eca51a908f4b1ba074639920200
Dava1 <davinci2189@gmail.com>
2021-04-01 14:43:27 +0200

    Dava1:add form for creating new messages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,43 @@
+/*reference to controller*/
 var messageApi = Vue.resource('/message{/id}');
+
+/*add form for adding new message*/
+Vue.component('message-form',{
+    props:['messages'],
+    data:function (){
+      return{
+          text:''
+      }
+    },
+    template:
+    '<div>' +
+       '<input type="text" placeholder="Write something"/>'+
+       '<input type="button" value="save" @click="save"/>'+
+       '</div>',
+    methods: {
+        save:function (){
+            var message ={text: this.text};
+            messageApi.save({},message).then(result=>
+            result.json().then(data=>{
+                this.messages.push(data);
+            })
+            );
+        }
+    }
+});
+
+/* descripe how to print one row from message */
 Vue.component('message-row',{
     props:['message'],
     template:'<div><i>({{message.id }})</i>{{message.text}}</div>'
 });
+
+/* describe hot to print our messages*/
 Vue.component('messages-list',{
     props:['messages'],
     template:
         '<div>' +
+            '<message-form :messages="messages"/>'+
             '<message-row v-for="message in messages" :key="message.id" :message="message"/>' +
         '</div>',
     created: function (){
@@ -17,6 +48,7 @@ Vue.component('messages-list',{
     }
 });
 
+/*print messages*/
 var app = new Vue({
     el: '#app',
     template:'<messages-list :messages="messages"/>',
